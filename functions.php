698a438363820dcb056c92a699d378b714f7f5f0
Francesco Bianco <info.francescobianco@gmail.com>
2021-10-28 19:04:58 +0200

    First commit!

--- functions.php
+++ functions.php
@@ -1,42 +1,46 @@
 <?php
 
-function utf8_sanitize($mixed)
-{
-    if (is_array($mixed)) {
-        foreach ($mixed as $key => $value) {
-            $mixed[$key] = utf8_sanitize($value);
+if (!function_exists('utf8_sanitize')) {
+    function utf8_sanitize($mixed)
+    {
+        if (is_array($mixed)) {
+            foreach ($mixed as $key => $value) {
+                $mixed[$key] = utf8_sanitize($value);
+            }
+        } elseif (is_object($mixed)) {
+            foreach ($mixed as $key => $value) {
+                $mixed->{$key} = utf8_sanitize($value);
+            }
+        } elseif (is_string($mixed)) {
+            return mb_convert_encoding($mixed, "UTF-8", "UTF-8");
         }
-    } elseif (is_object($mixed)) {
-        foreach ($mixed as $key => $value) {
-            $mixed->{$key} = utf8_sanitize($value);
-        }
-    } elseif (is_string($mixed)) {
-        return mb_convert_encoding($mixed, "UTF-8", "UTF-8");
-    }
 
-    return $mixed;
+        return $mixed;
+    }
 }
 
-function json_safe_encode($object, $flags = null)
-{
-    if (defined('JSON_INVALID_UTF8_IGNORE')) {
-        $flags |= JSON_INVALID_UTF8_IGNORE;
-    } else {
-        $object = utf8_sanitize($object);
+if (!function_exists('json_safe_encode')) {
+    function json_safe_encode($object, $flags = null)
+    {
+        if (defined('JSON_INVALID_UTF8_IGNORE')) {
+            $flags |= JSON_INVALID_UTF8_IGNORE;
+        } else {
+            $object = utf8_sanitize($object);
+        }
+
+        $flags |= JSON_PRETTY_PRINT;
+        $flags |= JSON_UNESCAPED_SLASHES;
+
+        $json = json_encode($object, $flags);
+
+        if ($json === false) {
+            $json = json_encode([
+                'json_last_error' => json_last_error(),
+                'json_last_error_msg' => json_last_error_msg(),
+                'trace' => debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)
+            ], JSON_PRETTY_PRINT);
+        }
+
+        return $json;
     }
-
-    $flags |= JSON_PRETTY_PRINT;
-    $flags |= JSON_UNESCAPED_SLASHES;
-
-    $json = json_encode($object, $flags);
-
-    if ($json === false) {
-        $json = json_encode([
-            'json_last_error' => json_last_error(),
-            'json_last_error_msg' => json_last_error_msg(),
-            'trace' => debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)
-        ], JSON_PRETTY_PRINT);
-    }
-
-    return $json;
 }
